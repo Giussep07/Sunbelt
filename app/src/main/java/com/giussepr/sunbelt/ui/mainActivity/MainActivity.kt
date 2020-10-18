86e0f271e17c69dfb578a2ad7ad80c872b57bfd1
Giussep Ricardo <giussep.naposystems@gmail.com>
2020-10-18 15:34:06 -0500

    - Implementación librería Navigation de Jetpack - Implementación icono de app

--- app/src/main/java/com/giussepr/sunbelt/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/giussepr/sunbelt/ui/mainActivity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.giussepr.sunbelt
+package com.giussepr.sunbelt.ui.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.giussepr.sunbelt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/giussepr/sunbelt/ui/splash/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.giussepr.sunbelt.ui.splash
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.giussepr.sunbelt.R
+
+class SplashFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SplashFragment()
+    }
+
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
